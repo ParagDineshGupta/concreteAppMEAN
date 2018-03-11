4cf3473b702206dd03b85e764a077f646f7916ec
Ubuntu <ubuntu@ip-172-31-35-81.us-east-2.compute.internal>
2018-03-11 08:00:07 +0000

    getsuppliername api response changed from user field to supplierName field

--- routes/index.js
+++ routes/index.js
@@ -221,7 +221,7 @@ router.post('/getsuppliername', function(req, res){
         if(user){
             return res.json({
                 success:true,
-                user: user.name
+                supplierName: user.name
             })
         }else{
             return res.json({
