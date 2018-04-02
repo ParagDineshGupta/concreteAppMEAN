1e387d4ef3059e6a024714784a7aa0e51947216b
Abhansh Giri <abhanshgiri21@gmail.com>
2018-04-02 21:14:47 +0530

    login url update

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -4,13 +4,13 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable()
 export class LoginService {
   
-  // private logInUrl = 'http://concrete.equipshare.in/api/users/login';
-  // private signupUrl = 'http://concrete.equipshare.in/api/users/signup';
-  // private cityUrl = 'http://concrete.equipshare.in/api/getcities';                 
+  private logInUrl = 'http://concrete.equipshare.in/api/users/login';
+  private signupUrl = 'http://concrete.equipshare.in/api/users/signup';
+  private cityUrl = 'http://concrete.equipshare.in/api/getcities';                 
 
-  private logInUrl = 'http://localhost:8080/api/users/login';
-  private signupUrl = 'http://localhost:8080/api/users/signup';
-  private cityUrl = 'http://localhost:8080/api/getcities';
+  // private logInUrl = 'http://localhost:8080/api/users/login';
+  // private signupUrl = 'http://localhost:8080/api/users/signup';
+  // private cityUrl = 'http://localhost:8080/api/getcities';
   
   constructor(private http: HttpClient) { }
 
